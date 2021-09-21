ae28597d8ecbdf01167f0e26c4b2b2bcef44e417
meyerc2 <MeyerC2@stu.otc.school.nz>
2021-09-22 09:37:51 +1200

    I have made my first and second window, now i need to make that window pretty.

--- Testing.py
+++ Testing.py
@@ -1,7 +1,10 @@
 import tkinter as tk                # python 3
+from tkinter import font as tkfont
+
+LARGE_FONT = ("Verdana", 12)
 
 
-class SampleApp(tk.Tk):
+class Notepad_Plus(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -15,9 +18,18 @@ class SampleApp(tk.Tk):
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
+        windowWidth = container.winfo_reqwidth()
+        windowHeight = container.winfo_reqheight()
+
+        # Gets both half the screen width/height and window width/height
+        positionRight = int(container.winfo_screenwidth()/2 - windowWidth/2)
+        positionDown = int(container.winfo_screenheight()/2 - windowHeight/2)
+
+        # Positions the window in the center of the page.
+        container.geometry("+{}+{}".format(positionRight, positionDown))
 
         self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, PageOne):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -50,7 +62,6 @@ class StartPage(tk.Frame):
         button1.pack()
         button2.pack()
 
-
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -62,19 +73,6 @@ class PageOne(tk.Frame):
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
 
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 2", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
-
-
 if __name__ == "__main__":
-    app = SampleApp()
+    app = Notepad_Plus()
     app.mainloop()
